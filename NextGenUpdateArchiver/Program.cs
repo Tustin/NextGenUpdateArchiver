71559e248cd6b06ef5b33ac4698093d5e964faf6
Tustin <Tustin@users.noreply.github.com>
2020-10-11 15:31:41 -0500

    Add project files.

---
+++ NextGenUpdateArchiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NextGenUpdateArchiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
